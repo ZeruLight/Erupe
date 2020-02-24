9182097411a135950a0ca622cd51d14ce757bdf2
Andrew Gutekanst <andrew.gutekanst@gmail.com>
2020-02-23 20:30:20 -0500

    Add MsgMhfAcquireDistItem size parser

--- network/mhfpacket/msg_mhf_acquire_dist_item.go
+++ network/mhfpacket/msg_mhf_acquire_dist_item.go
@@ -6,7 +6,12 @@ import (
 )
 
 // MsgMhfAcquireDistItem represents the MSG_MHF_ACQUIRE_DIST_ITEM
-type MsgMhfAcquireDistItem struct{}
+type MsgMhfAcquireDistItem struct {
+	AckHandle uint32
+
+	// Valid field size(s), not sure about the types.
+	Unk0 uint32
+}
 
 // Opcode returns the ID associated with this packet type.
 func (m *MsgMhfAcquireDistItem) Opcode() network.PacketID {
@@ -15,7 +20,9 @@ func (m *MsgMhfAcquireDistItem) Opcode() network.PacketID {
 
 // Parse parses the packet from binary
 func (m *MsgMhfAcquireDistItem) Parse(bf *byteframe.ByteFrame) error {
-	panic("Not implemented")
+	m.AckHandle = bf.ReadUint32()
+	m.Unk0 = bf.ReadUint32()
+	return nil
 }
 
 // Build builds a binary packet from the current data.

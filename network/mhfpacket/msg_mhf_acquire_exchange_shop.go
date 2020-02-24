6e6183022df9011dc8989494b7bc63aac122332c
Andrew Gutekanst <andrew.gutekanst@gmail.com>
2020-02-23 20:30:43 -0500

    Add MsgMhfAcquireExchangeShop parser

--- network/mhfpacket/msg_mhf_acquire_exchange_shop.go
+++ network/mhfpacket/msg_mhf_acquire_exchange_shop.go
@@ -6,7 +6,11 @@ import (
 )
 
 // MsgMhfAcquireExchangeShop represents the MSG_MHF_ACQUIRE_EXCHANGE_SHOP
-type MsgMhfAcquireExchangeShop struct{}
+type MsgMhfAcquireExchangeShop struct {
+	AckHandle      uint32
+	DataSize       uint16
+	RawDataPayload []byte
+}
 
 // Opcode returns the ID associated with this packet type.
 func (m *MsgMhfAcquireExchangeShop) Opcode() network.PacketID {
@@ -15,7 +19,10 @@ func (m *MsgMhfAcquireExchangeShop) Opcode() network.PacketID {
 
 // Parse parses the packet from binary
 func (m *MsgMhfAcquireExchangeShop) Parse(bf *byteframe.ByteFrame) error {
-	panic("Not implemented")
+	m.AckHandle = bf.ReadUint32()
+	m.DataSize = bf.ReadUint16()
+	m.RawDataPayload = bf.ReadBytes(uint(m.DataSize))
+	return nil
 }
 
 // Build builds a binary packet from the current data.

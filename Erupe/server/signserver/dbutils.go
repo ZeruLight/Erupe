29a7e5a6d9b311b7dd6baed7d01b161382c4d421
wishu <wish@sek.ai>
2022-04-24 07:08:34 +1000

    Launcher Patch

--- Erupe/server/signserver/dbutils.go
+++ Erupe/server/signserver/dbutils.go
@@ -6,6 +6,39 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+func (s *Server) newUserChara(username string) error {
+	var id int
+	err := s.db.QueryRow("SELECT id FROM users WHERE username = $1", username).Scan(&id)
+	if err != nil {
+		return err
+	}
+
+	var numNewChars int
+	err = s.db.QueryRow("SELECT COUNT(*) FROM characters WHERE user_id = $1 AND is_new_character = true", id).Scan(&numNewChars)
+	if err != nil {
+		return err
+	}
+
+	// prevent users with an uninitialised character from creating more
+	if numNewChars >= 1 {
+		return err
+	}
+
+	_, err = s.db.Exec(`
+		INSERT INTO characters (
+			user_id, is_female, is_new_character, small_gr_level, gr_override_mode, name, unk_desc_string,
+			gr_override_level, gr_override_unk0, gr_override_unk1, exp, weapon, last_login)
+		VALUES($1, False, True, 0, True, '', '', 0, 0, 0, 0, 0, $2)`,
+		id,
+		uint32(time.Now().Unix()),
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func (s *Server) registerDBAccount(username string, password string) error {
 	// Create salted hash of user password
 	passwordHash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)

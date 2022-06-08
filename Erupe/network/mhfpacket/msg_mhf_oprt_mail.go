526cec364a92b3069fad07d3435d479022f87ddf
wishu <wish@sek.ai>
2022-06-08 10:52:56 +1000

    implement mail locking

--- Erupe/network/mhfpacket/msg_mhf_oprt_mail.go
+++ Erupe/network/mhfpacket/msg_mhf_oprt_mail.go
@@ -12,12 +12,14 @@ type OperateMailOperation uint8
 
 const (
 	OPERATE_MAIL_DELETE = 0x01
+  OPERATE_MAIL_LOCK = 0x02
+  OPERATE_MAIL_UNLOCK = 0x03
   OPERATE_MAIL_ACQUIRE_ITEM = 0x05
 )
 
 // MsgMhfOprtMail represents the MSG_MHF_OPRT_MAIL
 type MsgMhfOprtMail struct {
-  AckHandle uint32
+	AckHandle uint32
 	AccIndex  uint8
 	Index     uint8
 	Operation OperateMailOperation
@@ -34,7 +36,7 @@ func (m *MsgMhfOprtMail) Opcode() network.PacketID {
 
 // Parse parses the packet from binary
 func (m *MsgMhfOprtMail) Parse(bf *byteframe.ByteFrame, ctx *clientctx.ClientContext) error {
-  m.AckHandle = bf.ReadUint32()
+	m.AckHandle = bf.ReadUint32()
 	m.AccIndex = bf.ReadUint8()
 	m.Index = bf.ReadUint8()
 	m.Operation = OperateMailOperation(bf.ReadUint8())

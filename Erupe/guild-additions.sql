3d68b5c6fd37574634b4d70b378ca090e29cec00
wishu <wish@sek.ai>
2022-06-16 16:23:42 +1000

    correct errors in guild schema

--- Erupe/guild-additions.sql
+++ Erupe/guild-additions.sql
@@ -2,16 +2,16 @@ BEGIN;
 
 ALTER TABLE IF EXISTS public.guilds
 (
-    ADD COLUMN pugi_name_1 character varying(12),
-    ADD COLUMN pugi_name_2 character varying(12),
-    ADD COLUMN pugi_name_3 character varying(12)
+    ADD COLUMN pugi_name_1 varchar(12),
+    ADD COLUMN pugi_name_2 varchar(12),
+    ADD COLUMN pugi_name_3 varchar(12)
 );
 
 CREATE TABLE IF NOT EXISTS public.guild_alliances
 (
 	  id serial NOT NULL PRIMARY KEY,
-    name character varying(24) NOT NULL,
-    created_at timestamp without time zone NOT NULL,
+    name varchar(24) NOT NULL,
+    created_at timestamp without time zone NOT NULL DEFAULT now(),
     parent_id int NOT NULL,
     sub1_id int,
     sub2_id int

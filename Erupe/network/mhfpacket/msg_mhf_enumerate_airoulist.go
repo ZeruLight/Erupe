a45b8175bed41b40eef576bddb9752fb95c032b4
wishu <wish@sek.ai>
2022-07-08 10:09:07 +1000

    fix enumerate airoulist parsing

--- Erupe/network/mhfpacket/msg_mhf_enumerate_airoulist.go
+++ Erupe/network/mhfpacket/msg_mhf_enumerate_airoulist.go
@@ -1,7 +1,7 @@
 package mhfpacket
 
-import ( 
- "errors" 
+import (
+ "errors"
 
  	"erupe-ce/network/clientctx"
 	"erupe-ce/network"
@@ -11,6 +11,8 @@ import (
 // MsgMhfEnumerateAiroulist represents the MSG_MHF_ENUMERATE_AIROULIST
 type MsgMhfEnumerateAiroulist struct {
 	AckHandle uint32
+  Unk0 uint16
+  Unk1 uint16
 }
 
 // Opcode returns the ID associated with this packet type.
@@ -21,6 +23,8 @@ func (m *MsgMhfEnumerateAiroulist) Opcode() network.PacketID {
 // Parse parses the packet from binary
 func (m *MsgMhfEnumerateAiroulist) Parse(bf *byteframe.ByteFrame, ctx *clientctx.ClientContext) error {
 	m.AckHandle = bf.ReadUint32()
+  m.Unk0 = bf.ReadUint16()
+  m.Unk1 = bf.ReadUint16()
 	return nil
 }
 

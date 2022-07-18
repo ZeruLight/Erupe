350301f7d3272be28291a43c07f1a5ae53090214
wishu <wish@sek.ai>
2022-07-18 15:18:12 +1000

    merge guild schema

--- Erupe/guild-additions.sql
+++ Erupe/guild-additions.sql
@@ -17,4 +17,24 @@ CREATE TABLE IF NOT EXISTS public.guild_alliances
     sub2_id int
 );
 
+CREATE TABLE IF NOT EXISTS public.guild_adventures
+(
+    id serial NOT NULL PRIMARY KEY,
+    guild_id int NOT NULL,
+    destination int NOT NULL,
+    charge int NOT NULL DEFAULT 0,
+    depart int NOT NULL,
+    return int NOT NULL,
+    collected_by text NOT NULL DEFAULT ''
+);
+
+CREATE TABLE IF NOT EXISTS public.guild_meals
+(
+	  id serial NOT NULL PRIMARY KEY,
+    guild_id int NOT NULL,
+    meal_id int NOT NULL,
+    level int NOT NULL,
+    expires int NOT NULL
+);
+
 END;

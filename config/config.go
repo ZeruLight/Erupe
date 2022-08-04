ed11b5ced975765b526b94defe7293b9a10a34e9
wish <wish@sek.ai>
2022-08-04 10:51:31 +1000

    implement token verification

--- config/config.go
+++ config/config.go
@@ -36,6 +36,7 @@ type DevModeOptions struct {
 	FestaEvent          int    // Hunter's Festa event status
 	TournamentEvent     int    // VS Tournament event status
 	MezFesEvent         bool   // MezFes status
+	DisableTokenCheck   bool   // Disables checking login token exists in the DB (security risk!)
 	DisableMailItems    bool   // Hack to prevent english versions of MHF from crashing
 	SaveDumps           SaveDumpOptions
 }

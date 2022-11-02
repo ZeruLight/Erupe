ada593990e6e395104b4edeedc20be7457982730
wish <wish@sek.ai>
2022-11-02 19:13:23 +1100

    update CheckMonthlyItem

--- network/mhfpacket/msg_mhf_check_monthly_item.go
+++ network/mhfpacket/msg_mhf_check_monthly_item.go
@@ -11,7 +11,8 @@ import (
 // MsgMhfCheckMonthlyItem represents the MSG_MHF_CHECK_MONTHLY_ITEM
 type MsgMhfCheckMonthlyItem struct {
 	AckHandle uint32
-	Unk       uint32
+	Type      uint8
+	Unk       []byte
 }
 
 // Opcode returns the ID associated with this packet type.
@@ -22,7 +23,8 @@ func (m *MsgMhfCheckMonthlyItem) Opcode() network.PacketID {
 // Parse parses the packet from binary
 func (m *MsgMhfCheckMonthlyItem) Parse(bf *byteframe.ByteFrame, ctx *clientctx.ClientContext) error {
 	m.AckHandle = bf.ReadUint32()
-	m.Unk = bf.ReadUint32()
+	m.Type = bf.ReadUint8()
+	m.Unk = bf.ReadBytes(3)
 	return nil
 }
 

6605c6f28ae477d7d606e88e3ef71b0800f69dab
wish <wish@sek.ai>
2022-11-07 00:30:24 +1100

    rearrange config options

--- server/signserver/dsgn_resp.go
+++ server/signserver/dsgn_resp.go
@@ -35,7 +35,7 @@ func (s *Session) makeSignInResp(uid int) []byte {
 	bf := byteframe.NewByteFrame()
 
 	bf.WriteUint8(1) // resp_code
-	if s.server.erupeConfig.DevMode && s.server.erupeConfig.DevModeOptions.PatchServerManifest != "" && s.server.erupeConfig.DevModeOptions.PatchServerFile != "" {
+	if s.server.erupeConfig.DevMode && s.server.erupeConfig.PatchServerManifest != "" && s.server.erupeConfig.PatchServerFile != "" {
 		bf.WriteUint8(2)
 	} else {
 		bf.WriteUint8(0)
@@ -46,9 +46,9 @@ func (s *Session) makeSignInResp(uid int) []byte {
 	bf.WriteBytes([]byte(sessToken))          // login_token
 	bf.WriteUint32(uint32(time.Now().Unix())) // current time
 	if s.server.erupeConfig.DevMode {
-		if s.server.erupeConfig.DevModeOptions.PatchServerManifest != "" && s.server.erupeConfig.DevModeOptions.PatchServerFile != "" {
-			ps.Uint8(bf, s.server.erupeConfig.DevModeOptions.PatchServerManifest, false)
-			ps.Uint8(bf, s.server.erupeConfig.DevModeOptions.PatchServerFile, false)
+		if s.server.erupeConfig.PatchServerManifest != "" && s.server.erupeConfig.PatchServerFile != "" {
+			ps.Uint8(bf, s.server.erupeConfig.PatchServerManifest, false)
+			ps.Uint8(bf, s.server.erupeConfig.PatchServerFile, false)
 		}
 	}
 	ps.Uint8(bf, fmt.Sprintf("%s:%d", s.server.erupeConfig.Host, s.server.erupeConfig.Entrance.Port), false)

e98259a11a8c0e8d661c1aa316085fac4fdc735a
wish <wish@sek.ai>
2023-02-13 00:21:07 +1100

    move various packet handlers

--- network/mhfpacket/msg_mhf_get_weekly_seibatu_ranking_reward.go
+++ network/mhfpacket/msg_mhf_get_weekly_seibatu_ranking_reward.go
@@ -1,15 +1,21 @@
 package mhfpacket
 
-import ( 
- "errors" 
+import (
+	"errors"
 
- 	"erupe-ce/network/clientctx"
-	"erupe-ce/network"
 	"erupe-ce/common/byteframe"
+	"erupe-ce/network"
+	"erupe-ce/network/clientctx"
 )
 
 // MsgMhfGetWeeklySeibatuRankingReward represents the MSG_MHF_GET_WEEKLY_SEIBATU_RANKING_REWARD
-type MsgMhfGetWeeklySeibatuRankingReward struct{}
+type MsgMhfGetWeeklySeibatuRankingReward struct {
+	AckHandle uint32
+	Unk0      uint32
+	Unk1      uint32
+	Unk2      uint32
+	Unk3      uint32
+}
 
 // Opcode returns the ID associated with this packet type.
 func (m *MsgMhfGetWeeklySeibatuRankingReward) Opcode() network.PacketID {
@@ -18,7 +24,12 @@ func (m *MsgMhfGetWeeklySeibatuRankingReward) Opcode() network.PacketID {
 
 // Parse parses the packet from binary
 func (m *MsgMhfGetWeeklySeibatuRankingReward) Parse(bf *byteframe.ByteFrame, ctx *clientctx.ClientContext) error {
-	return errors.New("NOT IMPLEMENTED")
+	m.AckHandle = bf.ReadUint32()
+	m.Unk0 = bf.ReadUint32()
+	m.Unk1 = bf.ReadUint32()
+	m.Unk2 = bf.ReadUint32()
+	m.Unk3 = bf.ReadUint32()
+	return nil
 }
 
 // Build builds a binary packet from the current data.
--- network/mhfpacket/msg_mhf_present_box.go
+++ network/mhfpacket/msg_mhf_present_box.go
@@ -1,15 +1,26 @@
 package mhfpacket
 
-import ( 
- "errors" 
+import (
+	"errors"
 
- 	"erupe-ce/network/clientctx"
-	"erupe-ce/network"
 	"erupe-ce/common/byteframe"
+	"erupe-ce/network"
+	"erupe-ce/network/clientctx"
 )
 
 // MsgMhfPresentBox represents the MSG_MHF_PRESENT_BOX
-type MsgMhfPresentBox struct{}
+type MsgMhfPresentBox struct {
+	AckHandle uint32
+	Unk0      uint32
+	Unk1      uint32
+	Unk2      uint32
+	Unk3      uint32
+	Unk4      uint32
+	Unk5      uint32
+	Unk6      uint32
+	Unk7      uint32
+	Unk8      uint32
+}
 
 // Opcode returns the ID associated with this packet type.
 func (m *MsgMhfPresentBox) Opcode() network.PacketID {
@@ -18,7 +29,17 @@ func (m *MsgMhfPresentBox) Opcode() network.PacketID {
 
 // Parse parses the packet from binary
 func (m *MsgMhfPresentBox) Parse(bf *byteframe.ByteFrame, ctx *clientctx.ClientContext) error {
-	return errors.New("NOT IMPLEMENTED")
+	m.AckHandle = bf.ReadUint32()
+	m.Unk0 = bf.ReadUint32()
+	m.Unk1 = bf.ReadUint32()
+	m.Unk2 = bf.ReadUint32()
+	m.Unk3 = bf.ReadUint32()
+	m.Unk4 = bf.ReadUint32()
+	m.Unk5 = bf.ReadUint32()
+	m.Unk6 = bf.ReadUint32()
+	m.Unk7 = bf.ReadUint32()
+	m.Unk8 = bf.ReadUint32()
+	return nil
 }
 
 // Build builds a binary packet from the current data.

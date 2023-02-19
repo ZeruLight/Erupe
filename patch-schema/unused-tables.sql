bbaec08b67199d3abdb962a66bd55f6be94b8ca4
wish <wish@sek.ai>
2023-02-20 00:34:39 +1100

    remove unused database tables

---
+++ patch-schema/unused-tables.sql
@@ -0,0 +1,17 @@
+BEGIN;
+
+DROP TABLE IF EXISTS public.account_ban;
+
+DROP TABLE IF EXISTS public.account_history;
+
+DROP TABLE IF EXISTS public.account_moderation;
+
+DROP TABLE IF EXISTS public.account_sub;
+
+DROP TABLE IF EXISTS public.history;
+
+DROP TABLE IF EXISTS public.questlists;
+
+DROP TABLE IF EXISTS public.schema_migrations;
+
+END;

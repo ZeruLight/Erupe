051f2d2a4dd1c5dab4adf195ef5369b8c0241d4e
wish <wish@sek.ai>
2023-03-11 00:11:10 +1100

    parse GetRyoudama correctly

--- network/mhfpacket/msg_mhf_get_ryoudama.go
+++ network/mhfpacket/msg_mhf_get_ryoudama.go
@@ -1,19 +1,20 @@
 package mhfpacket
 
-import ( 
- "errors" 
+import (
+	"errors"
 
- 	"erupe-ce/network/clientctx"
-	"erupe-ce/network"
 	"erupe-ce/common/byteframe"
+	"erupe-ce/network"
+	"erupe-ce/network/clientctx"
 )
 
 // MsgMhfGetRyoudama represents the MSG_MHF_GET_RYOUDAMA
-type MsgMhfGetRyoudama struct{
-	AckHandle      uint32
-	Unk0      		 uint16
-	Unk1      		 uint32
-	Unk2      		 uint8
+type MsgMhfGetRyoudama struct {
+	AckHandle uint32
+	Unk0      uint8
+	Unk1      uint8
+	GuildID   uint32
+	Unk3      uint8
 }
 
 // Opcode returns the ID associated with this packet type.
@@ -24,9 +25,10 @@ func (m *MsgMhfGetRyoudama) Opcode() network.PacketID {
 // Parse parses the packet from binary
 func (m *MsgMhfGetRyoudama) Parse(bf *byteframe.ByteFrame, ctx *clientctx.ClientContext) error {
 	m.AckHandle = bf.ReadUint32()
-	m.Unk0 = bf.ReadUint16()
-	m.Unk1 = bf.ReadUint32()
-	m.Unk2 = bf.ReadUint8()
+	m.Unk0 = bf.ReadUint8()
+	m.Unk1 = bf.ReadUint8()
+	m.GuildID = bf.ReadUint32()
+	m.Unk3 = bf.ReadUint8()
 	return nil
 }
 

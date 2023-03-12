c344fdd4c9b5605369e549ee22dccf5711ccb300
wish <wish@sek.ai>
2023-03-12 23:43:30 +1100

    fix scout enumeration data

--- server/channelserver/handlers_guild_scout.go
+++ server/channelserver/handlers_guild_scout.go
@@ -204,7 +204,7 @@ func handleMsgMhfGetGuildScoutList(s *Session, p mhfpacket.MHFPacket) {
 	}
 
 	rows, err := s.server.db.Queryx(`
-		SELECT c.id, c.name, ga.actor_id
+		SELECT c.id, c.name, c.hrp, c.gr, ga.actor_id
 			FROM guild_applications ga 
 			JOIN characters c ON c.id = ga.character_id
 		WHERE ga.guild_id = $1 AND ga.application_type = 'invited'
@@ -229,14 +229,14 @@ func handleMsgMhfGetGuildScoutList(s *Session, p mhfpacket.MHFPacket) {
 
 	for rows.Next() {
 		var charName string
-		var charID uint32
-		var actorID uint32
+		var charID, actorID uint32
+		var hrp, gr uint16
 
-		err = rows.Scan(&charID, &charName, &actorID)
+		err = rows.Scan(&charID, &charName, &hrp, &gr, &actorID)
 
 		if err != nil {
 			doAckSimpleFail(s, pkt.AckHandle, nil)
-			panic(err)
+			continue
 		}
 
 		// This seems to be used as a unique ID for the invitation sent
@@ -246,8 +246,8 @@ func handleMsgMhfGetGuildScoutList(s *Session, p mhfpacket.MHFPacket) {
 		bf.WriteUint32(actorID)
 		bf.WriteUint32(charID)
 		bf.WriteUint32(uint32(TimeAdjusted().Unix()))
-		bf.WriteUint16(0x00) // HR?
-		bf.WriteUint16(0x00) // GR?
+		bf.WriteUint16(hrp) // HR?
+		bf.WriteUint16(gr)  // GR?
 		bf.WriteBytes(stringsupport.PaddedString(charName, 32, true))
 		count++
 	}

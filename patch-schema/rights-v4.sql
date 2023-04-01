f0db7f0a19163fa3161f4ee8e520c61cf414a886
wish <wish@sek.ai>
2023-04-01 17:58:26 +1100

    finalise 9.2

--- patch-schema/rights-v4.sql
+++
@@ -1,9 +0,0 @@
-BEGIN;
-
--- Remove Trial Course from all users
-UPDATE users SET rights = rights-2;
-
-ALTER TABLE IF EXISTS public.users
-    ALTER COLUMN rights SET DEFAULT 12;
-
-END;
--- patch-schema/stampcard.sql
+++
@@ -1,5 +0,0 @@
-BEGIN;
-
-ALTER TABLE characters ADD stampcard INT NOT NULL DEFAULT 0;
-
-END;

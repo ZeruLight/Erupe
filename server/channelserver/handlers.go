99919d0243411d30c2f1a5f0b434911f773312d0
wish <wish@sek.ai>
2023-05-23 21:55:32 +1000

    remove unused value

--- server/channelserver/handlers.go
+++ server/channelserver/handlers.go
@@ -1612,7 +1612,7 @@ func handleMsgMhfGetEarthStatus(s *Session, p mhfpacket.MHFPacket) {
 	bf.WriteUint32(uint32(TimeWeekStart().Unix())) // Start
 	bf.WriteUint32(uint32(TimeWeekNext().Unix()))  // End
 	bf.WriteInt32(s.server.erupeConfig.DevModeOptions.EarthStatusOverride)
-	bf.WriteInt32(21)
+	bf.WriteInt32(0)
 	bf.WriteInt32(0)
 	bf.WriteInt32(0)
 	bf.WriteInt32(0)

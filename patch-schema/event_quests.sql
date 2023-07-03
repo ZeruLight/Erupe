e29ce1d82f5132b7e68e7468915904489c454416
wish <wish@sek.ai>
2023-07-03 20:33:03 +1000

    fix events_quests id missing PK flag

--- patch-schema/event_quests.sql
+++ patch-schema/event_quests.sql
@@ -2,7 +2,7 @@ BEGIN;
 
 create table if not exists event_quests
 (
-    id          serial,
+    id          serial primary key,
     max_players integer,
     quest_type  integer,
     quest_id    uint16 not null

f5b40b55b9a84a661227add5faabe57a403a0f12
Ewerton B. S <malckyormetal@gmail.com>
2023-08-20 08:00:39 +0900

    Update 03-event_quests.sql for auto-cycle

--- patch-schema/03-event_quests.sql
+++ patch-schema/03-event_quests.sql
@@ -6,9 +6,17 @@ create table if not exists event_quests
     max_players integer,
     quest_type integer not null,
     quest_id integer not null,
-    mark integer
+    mark integer,
+    weekly_cycle INT,
+    available_in_all_cycles bool default true
 );
 
 ALTER TABLE IF EXISTS public.servers DROP COLUMN IF EXISTS season;
+
+CREATE TABLE IF NOT EXISTS weekly_cycle_info (
+    id SERIAL PRIMARY KEY,
+    current_cycle_number INT,
+    last_cycle_update_timestamp TIMESTAMP WITH TIME ZONE
+);
 
 END;

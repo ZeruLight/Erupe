c0d11bea4bc113abcd88591050e21e4171d30ac2
Ewerton B. S <malckyormetal@gmail.com>
2023-08-21 03:22:45 +0900

    Update config.go auto-cycle

--- config/config.go
+++ config/config.go
@@ -111,7 +111,8 @@ type DevModeOptions struct {
 	EarthStatusOverride  int32
 	EarthIDOverride      int32
 	EarthMonsterOverride int32
-	WeeklyQuestCycle     bool
+	WeeklyQuestCycle     bool // Enable weekly event quests cycle
+	WeeklyCycleAmount    int  // Set the amount of cycles (week)
 	SaveDumps            SaveDumpOptions
 }
 

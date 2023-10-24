64cb106c70f877545e7b519fbf9e85861c768ae5
wish <wish@sek.ai>
2023-10-24 22:18:09 +1100

    formatting, strip out unknown values

--- patch-schema/03-event_quests.sql
+++ patch-schema/03-event_quests.sql
@@ -3,14 +3,10 @@ BEGIN;
 create table if not exists event_quests
 (
     id serial primary key,
-    index_value integer,
     max_players integer,
     quest_type integer not null,
     quest_id integer not null,
-    mark integer,
-    quest_option integer not null,
-
-
+    mark integer
 );
 
 ALTER TABLE IF EXISTS public.servers DROP COLUMN IF EXISTS season;

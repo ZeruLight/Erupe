3b33c1917c82247ee65fb98849b33380c4294ea2
Matthew <matthew.elswick@matthe815.dev>
2023-11-19 13:52:03 -0500

    sql: Remove new migration code from old patch-schema

--- patch-schema/03-event_quests.sql
+++ patch-schema/03-event_quests.sql
@@ -6,10 +6,7 @@ create table if not exists event_quests
     max_players integer,
     quest_type integer not null,
     quest_id integer not null,
-    mark integer,
-    start_time timestamp with time zone NOT NULL DEFAULT (CURRENT_DATE + interval '0 second'),
-    active_duration int not null,
-    inactive_duration int not null
+    mark integer
 );
 
 ALTER TABLE IF EXISTS public.servers DROP COLUMN IF EXISTS season;

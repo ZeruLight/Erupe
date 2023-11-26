a0970de48c139871dc6474fb112018ccb66c24d8
wish <wish@sek.ai>
2023-11-26 13:02:17 +1100

    review pass for Event Quest cycling

--- patch-schema/12-event_quest_cycling.sql
+++ patch-schema/12-event_quest_cycling.sql
@@ -1,7 +1,8 @@
 BEGIN;
 
-ALTER TABLE IF EXISTS public.event_quests ADD COLUMN IF NOT EXISTS start_time timestamp with time zone NOT NULL DEFAULT (CURRENT_DATE + interval '0 second');
-ALTER TABLE IF EXISTS public.event_quests ADD COLUMN IF NOT EXISTS active_duration int DEFAULT 4;
-ALTER TABLE IF EXISTS public.event_quests ADD COLUMN IF NOT EXISTS inactive_duration int DEFAULT 3;
+ALTER TABLE IF EXISTS public.event_quests ADD COLUMN IF NOT EXISTS start_time timestamp with time zone NOT NULL DEFAULT now();
+ALTER TABLE IF EXISTS public.event_quests ADD COLUMN IF NOT EXISTS active_duration int;
+ALTER TABLE IF EXISTS public.event_quests ADD COLUMN IF NOT EXISTS inactive_duration int;
+UPDATE public.event_quests SET active_duration=NULL, inactive_duration=NULL;
 
 END;

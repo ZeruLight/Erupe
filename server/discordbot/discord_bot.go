4cf8b6b4ab7405253252dc75e7b82f6643a5ebdd
Brent <75487564+Brentdbr@users.noreply.github.com>
2024-03-05 11:54:53 +0100

    Update discord_bot.go to stop crashing
    It will just continue crashing so long as it allows anything other than ASCII characters at some point when you enter a string of them that ends up messy. katakana, hiragana and han should therefore not be allowed until a fix for that is found.

--- server/discordbot/discord_bot.go
+++ server/discordbot/discord_bot.go
@@ -87,7 +87,7 @@ func (bot *DiscordBot) Start() (err error) {
 func (bot *DiscordBot) NormalizeDiscordMessage(message string) string {
 	userRegex := regexp.MustCompile(`<@!?(\d{17,19})>`)
 	emojiRegex := regexp.MustCompile(`(?:<a?)?:(\w+):(?:\d{18}>)?`)
-	messageRegex := regexp.MustCompile(`[^\p{Hiragana}\p{Katakana}\p{Han}\p{P}\p{S}\x{00}-\x{7E}]`)
+	messageRegex := regexp.MustCompile(`[^\x{20}-\x{7E}]`)
 
 	result := ReplaceTextAll(message, userRegex, func(userId string) string {
 		user, err := bot.Session.User(userId)

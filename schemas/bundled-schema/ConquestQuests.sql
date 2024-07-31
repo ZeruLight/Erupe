40b73baf75c41370fde0210e6486a113f36519e2
stratic-dev <straticspaff@gmail.com>
2024-07-31 18:26:11 +0100

    Added conquest quests

---
+++ schemas/bundled-schema/ConquestQuests.sql
@@ -0,0 +1,9 @@
+BEGIN;
+
+INSERT INTO public.event_quests (max_players, quest_type, quest_id, mark) VALUES
+    (0,33,54257,0),
+    (0,33,54258,0),
+    (0,33,54277,0),
+    (0,33,54370,0);
+
+END;

2d0cce719ba9b7766937cda3e1ed88f610d189d1
stratic-dev <straticspaff@gmail.com>
2024-07-31 18:06:32 +0100

    Added seiabatu cross event pkt

--- network/mhfpacket/msg_mhf_get_weekly_seibatu_ranking_reward.go
+++ network/mhfpacket/msg_mhf_get_weekly_seibatu_ranking_reward.go
@@ -2,6 +2,7 @@ package mhfpacket
 
 import (
 	"errors"
+	"fmt"
 
 	"erupe-ce/common/byteframe"
 	"erupe-ce/network"
@@ -10,11 +11,11 @@ import (
 
 // MsgMhfGetWeeklySeibatuRankingReward represents the MSG_MHF_GET_WEEKLY_SEIBATU_RANKING_REWARD
 type MsgMhfGetWeeklySeibatuRankingReward struct {
-	AckHandle uint32
-	Unk0      uint32
-	Unk1      uint32
-	Unk2      uint32
-	Unk3      uint32
+	AckHandle    uint32
+	Unk0         uint32
+	Operation    uint32
+	ID           uint32
+	EarthMonster uint32
 }
 
 // Opcode returns the ID associated with this packet type.
@@ -26,9 +27,10 @@ func (m *MsgMhfGetWeeklySeibatuRankingReward) Opcode() network.PacketID {
 func (m *MsgMhfGetWeeklySeibatuRankingReward) Parse(bf *byteframe.ByteFrame, ctx *clientctx.ClientContext) error {
 	m.AckHandle = bf.ReadUint32()
 	m.Unk0 = bf.ReadUint32()
-	m.Unk1 = bf.ReadUint32()
-	m.Unk2 = bf.ReadUint32()
-	m.Unk3 = bf.ReadUint32()
+	m.Operation = bf.ReadUint32()
+	m.ID = bf.ReadUint32()
+	m.EarthMonster = bf.ReadUint32()
+	fmt.Printf("MsgMhfGetWeeklySeibatuRankingReward: Unk0:[%d] Operation:[%d] ID:[%d] EarthMonster:[%d]\n\n", m.Unk0, m.Operation, m.ID, m.EarthMonster)
 	return nil
 }
 

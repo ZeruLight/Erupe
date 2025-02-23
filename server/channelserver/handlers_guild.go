5028355cfcf3f2ad9f8fb4e65138c678e069594d
wish <wish@sek.ai>
2025-02-23 21:46:00 +1100

    prevent nil pointer in MhfGetGuildManageRight

--- server/channelserver/handlers_guild.go
+++ server/channelserver/handlers_guild.go
@@ -1526,7 +1526,7 @@ func handleMsgMhfGetGuildManageRight(s *Session, p mhfpacket.MHFPacket) {
 	pkt := p.(*mhfpacket.MsgMhfGetGuildManageRight)
 
 	guild, err := GetGuildInfoByCharacterId(s, s.charID)
-	if guild == nil && s.prevGuildID != 0 {
+	if guild == nil || s.prevGuildID != 0 {
 		guild, err = GetGuildInfoByID(s, s.prevGuildID)
 		s.prevGuildID = 0
 		if guild == nil || err != nil {
